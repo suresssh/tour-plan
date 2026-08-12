d6546dbf274efef80082a0590f23c8812f454981
suresssh <suresssh.s@gmail.com>
2026-08-12 10:48:56 +0530

    feat: error handling final

--- app.js
+++ app.js
@@ -1,46 +1,33 @@
 const express = require('express');
 const morgan = require('morgan');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const { tourRouter, userRouter } = require('./routes');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-
-dotenv.config({ path: './.env' });
+const {tourRouter, userRouter} = require('./routes');
 
 const app = express();
 
-const DB = process.env.DB_URI.replace('<db_password>', process.env.DB_PASSWORD);
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-mongoose.connect(DB, {}).then((con) => {
-  console.log('Db successfully connected');
-});
-
-// logger
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
-
+app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-//Middlewares
-app.use(express.json());
 app.use((req, res, next) => {
-  console.log('Hello from custom middleware');
+  req.requestTime = new Date().toISOString();
   next();
 });
 
-// use multiple routes
+// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter); 
+app.use('/api/v1/users', userRouter);
 
-// handle unhandled routes
-app.all('*', (req, res, next) => { // * is used to match all the routes
-  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  // err.statusCode = 404;
-  // err.status = 'fail';
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404)); // pass the error to the global error handling middleware
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-//  global error handling middleware
 app.use(globalErrorHandler);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,30 @@
-const app = require('./app');
-const port = process.env.PORT || 2000;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message,err.stack);
+  process.exit(1);
+});
+
+dotenv.config({ path: './.env' });
+const app = require('./app');
+
+const DB = process.env.DB_URI.replace('<db_password>', process.env.DB_PASSWORD);
+
+mongoose.connect(DB, {}).then((con) => {
+  console.log('Db successfully connected');
+});
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
